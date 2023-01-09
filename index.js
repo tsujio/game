8f72dbee4e2e93f844ac70b82f467908876a2356
tsujio <ntsujio@gmail.com>
2023-01-09 23:31:51 +0900

    Update

--- index.js
+++ index.js
@@ -3,13 +3,21 @@
     fetch("/games.json")
       .then((res) => res.json())
       .then((data) => {
+        const query = Object.fromEntries(location.search.substr(1,).split('&').map((kv) => kv.split('=')))
+
         const tmpl = document.querySelector("#game-container-template").content
         const gamesContainer = document.querySelector("#games-container")
 
         for (const game of data.games) {
           const container = document.importNode(tmpl, true)
           container.querySelector(".game-title").textContent = game.title
-          container.querySelector(".game-link").href = game.link
+
+          const link = new URL(game.link)
+          if (query.logging === "true" || query.logging === "false") {
+            link.searchParams.set("logging", query.logging)
+          }
+          container.querySelector(".game-link").href = link.toString()
+
           container.querySelector(".game-image").src = game.image
           container.querySelector(".game-description").textContent = game.description
 
